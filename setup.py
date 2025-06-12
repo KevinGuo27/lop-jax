43b8b9900dbef232c6ad12b78b1c2eb2288bc21c
kaicheng_guo@brown.edu <kguo32@vscode1.oscar.ccv.brown.edu>
2025-06-12 12:19:05 -0400

    update setup

--- setup.py
+++ setup.py
@@ -3,17 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="lop-jax",
     version="0.0.1",
-    packages=find_packages(),   # finds permuted_mnist, utils, etc.
-    install_requires=[
-        "jax",
-        "distrax",
-        "gymnax",
-        "typed-argument-parser",
-        "torch",
-        "torchvision",
-        "tqdm",
-        "optax",
-        "numpy==1.26.4",
-        "matplotlib",
-    ],
+    packages=find_packages(),
+    py_modules=["definitions"]
 )
